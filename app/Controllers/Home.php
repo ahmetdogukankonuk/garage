5b7a5ccf34f94802ea3a3900fcd5820a6c1126f7
ahmetdogukankonuk <ahmetdogukankonuk@gmail.com>
2023-07-01 20:38:04 +0300

    Template added to the project

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\VehiclesModel;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
@@ -9,11 +10,15 @@ class Home extends BaseController
 {
     public function index()
     {
+        $vehiclesModel = new VehiclesModel();
+        $vehicles = $vehiclesModel->getAllVehicles();
+
         // Twig template engine configuration
-        $loader = new FilesystemLoader(__DIR__ . '/../views'); // Assuming the Twig templates are in the "views" directory
+        $loader = new FilesystemLoader(APPPATH . 'Views');
         $twig = new Environment($loader);
 
-        $template = $twig->load('welcome_message.twig');
-        return $template->render(['the' => 'variables', 'go' => 'here']);
+        // Render the template and return the result
+        echo $twig->render('home.twig', ['vehicles' => $vehicles]);
     }
 }
+?>
